ad1e5c861277581618d15848caf6c9839e66feb9
Hussein El-Zein <91071020+HusseinElZein@users.noreply.github.com>
2023-12-06 12:45:20 +0100

    Initial Commit

---
+++ SwiftExercises/SwiftExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExercisesApp.swift
+//  SwiftExercises
+//
+//  Created by Hussein on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
